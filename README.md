5107a71aaf2517db8b15c375d8d646a09cbb4472
Luis Otávio <luis.otavio2425@gmail.com>
2020-06-18 14:13:06 -0300

    Add Readme

---
+++ README.md
@@ -0,0 +1,46 @@
+<h1  align="center">GoStack ReactJS Challenge #02</h1>
+    <img  src="https://res.cloudinary.com/luisotavio756/image/upload/v1592267094/60937329-69836380-a2a6-11e9-910b-759f9f1d26a7_mqbbok.png" />
+<h4  align="center">A challenge two from Bootcamp GoStack of the Rockeseat</h4>
+<p  align="center">
+    <a  href="#rocket-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+    <a  href="#information_source-how-to-use">How To Use</a>&nbsp;&nbsp;&nbsp;
+</p>
+
+## :rocket: Technologies
+
+In the project Web developed with React, i can list and create repositories using API REST make in chanllenge pass. Again, more one simple project that is aborded maney better ReactJS concepts.
+
+- [API REST Used](https://github.com/luisotavio756/gostack-desafio01)
+- [ReactJS](https://pt-br.reactjs.org/)
+
+## :information_source: How To Use
+
+For use, you need clone and run this steps from your command line:
+
+```bash
+
+# Clone this repository
+
+$ git clone https://github.com/luisotavio756/gostack-desafio02-react.git
+
+# Go into the repository
+
+$ cd gostack-desafio02-react
+
+# Install packages
+
+$ yarn install or npm install
+
+# Run start and enjoy
+
+$ yarn start or npm start
+
+# For test
+
+$ yarn test
+
+```
+
+---
+
+Made with ♥ by Luis Otávio and Rocketseat team :rocket:
--- src/App.js
+++ src/App.js
@@ -1,29 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+
+  useEffect(() => {
+    loadRepositories();
+  }, []);
+
+  async function loadRepositories() {
+    try {
+      const { data } = await api.get('/repositories');
+
+      setRepositories(data);
+    } catch (error) {
+      alert('Não foi possível buscar os repositórios !');
+    }
+  }
+
   async function handleAddRepository() {
-    // TODO
+    const data = {
+      title: "Repositório" + new Date(),
+      url: 'https://github.com/luisotavio756/gostack-desafio02.git',
+      techs: [
+        'ReactJS',
+        'Node.js'
+      ]
+    }
+
+    try {
+      const response = await api.post('/repositories', data);
+
+      setRepositories([
+        ...repositories,
+        response.data
+      ]);
+
+    } catch (error) {
+      alert('Não foi possível adicionar o repositório !');
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      const response = await api.delete(`/repositories/${id}`);
+
+      const updatedRepositories = repositories.filter(rep => rep.id !== id);
+
+      setRepositories(updatedRepositories);
+    } catch (error) {
+      alert('Não foi possível excluir o repositório !');
+    }
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <div id="container">
+      <div className="input-blocks">
+        <input 
+          name="title" 
+          type="text"
+          placeholder="Ex: desafio01-gostack"
+        />
+        <input 
+          name="url" 
+          type="text"
+          placeholder="Ex: https://github.com/luisotavio756/gostack-desafio02.git"
+        />
+        <input 
+          name="techs" 
+          type="text"
+          placeholder="Ex: ReactJS, Node.js"
+        />
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
+      <div className="list">
+        <ul data-testid="repository-list">
+          {
+            repositories.length > 0 ?
+            repositories.map(repositorie => (
+              <li key={repositorie.id}>
+                { repositorie.title }
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+                <button onClick={() => handleRemoveRepository(repositorie.id)}>
+                  Remover
+                </button>
+              </li>
+            )) : null
+          }
+        </ul>
+      </div>
+      <p style={{ marginTop: 10 }}>Made with ❤ por Luis Otávio</p>
     </div>
   );
 }
